48b3e3b6d3bcf3439534585701717901ab19e54c
Lacus <zhx35139@126.com>
2014-11-09 19:43:24 +0800

    Helloworld
    Helloworld

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Lacus on 14-11-9.
+//  Copyright (c) 2014年 Lacus. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main() {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
