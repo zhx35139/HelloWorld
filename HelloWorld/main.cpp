4074f77c0b38a4de2d5cc020b38937a1fa35cf84
Lacus <zhx35139@126.com>
2014-11-10 20:56:49 +0800

    test HelloWorld
    test

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -8,8 +8,17 @@
 
 #include <iostream>
 using namespace std;
+int f(int x){
+    if(x<0){
+        return 0;
+    }else if(x==0){
+        return 1;
+    }else{
+        return x*f(x-1);
+    }
+}
 int main() {
-    // insert code here...
-    cout << "Hello, World!\n";
+    int n = f(6);
+    cout << "n="<<n<<endl;
     return 0;
 }
